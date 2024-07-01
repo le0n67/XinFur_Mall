969a1b2a65b4d2537e6ce0f8612bb4747f0f391d
leon <le0n67@163.ccom>
2024-07-01 22:31:27 +0800

    register complete

--- src/com/leon/xinfur/web/RegisterServlet.java
+++ src/com/leon/xinfur/web/RegisterServlet.java
@@ -7,11 +7,16 @@ package com.leon.xinfur.web;
  * @version 1.0
  */
 
+import com.leon.xinfur.entity.Member;
+import com.leon.xinfur.service.MemberService;
+import com.leon.xinfur.service.impl.MemberServiceImpl;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class RegisterServlet extends HttpServlet {
+    MemberService memberService=new MemberServiceImpl();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
@@ -19,6 +24,32 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("register 被调用");
+        //用户名
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        //判断这个用户名是不是可用
+        if(!memberService.isExistsUserName(username)) {
+            //注册
+            //System.out.println("用户名 " + username + " 不存在, 可以注册");
+            //构建一个Member对象
+            Member member = new Member(null, username, password, email);
+            if(memberService.registerMember(member)) {
+                //请求转发
+                request.getRequestDispatcher("/views/member/register_ok.jsp")
+                        .forward(request,response);
+            } else  {
+                //请求转发
+                request.getRequestDispatcher("/views/member/register_fail.jsp")
+                        .forward(request,response);
+            }
+
+        } else  {
+            //返回注册页面
+            //后面可以加入提示信息...
+            request.getRequestDispatcher("/views/member/login.jsp")
+                    .forward(request,response);
+        }
     }
 }
